cd397b02b7d5354a041acf65a0d5660183b0f8c9
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-04-25 12:31:52 +0300

    Enhance build.h for logging and error handling

--- include/build.h
+++ include/build.h
@@ -1,4 +1,14 @@
 #pragma once
-#include <string>
 
-bool buildProject(const std::string &buildCommand);
+#include <string>
+#include <vector>
+
+// Log a message to the console
+void logMessage(const std::string &message);
+
+// Execute a command and return true if the command is successful (exit code is 0)
+bool executeCommand(const std::string &command);
+
+// Build the project by executing a list of build commands
+// Optionally, build commands can be executed in parallel if parallelExecution is set to true
+bool buildProject(const std::vector<std::string> &buildCommands, bool parallelExecution = false);
