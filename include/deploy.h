1b5265dbd36cf47d601a9cb84ad21087eaeaf7fa
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-04-25 13:20:13 +0300

    Enhance deployProject func to support multiple deployment commands and logging

--- include/deploy.h
+++ include/deploy.h
@@ -1,4 +1,7 @@
 #pragma once
-#include <string>
 
-bool deployProject(const std::string &deployCommand);
+#include <string>
+#include <vector>
+
+// Deploy the project using a list of deploy commands and return true if all deployments are successful
+bool deployProject(const std::vector<std::string> &deployCommands);
--- src/deploy.cpp
+++ src/deploy.cpp
@@ -1,13 +1,42 @@
 #include "deploy.h"
 #include <iostream>
 #include <cstdlib>
+#include <vector>
+#include <thread>
+#include <mutex>
 
+// Mutex for synchronizing log messages
+std::mutex log_mutex;
+
+// Log a message to the console, taking care of mutex synchronization
+// to avoid garbled log messages when logging from multiple threads
+void logMessage(const std::string &message) {
+    std::unique_lock<std::mutex> lock(log_mutex);
+    std::cout << message << std::endl;
+}
+
+// Execute a command and return true if the command is successful (exit code is 0)
+// Log the command execution and any errors that occur
 bool executeCommand(const std::string &command) {
+    logMessage("Executing command: " + command);
     int result = std::system(command.c_str());
+
+    if (result != 0) {
+        logMessage("Error: Command '" + command + "' failed with error code " + std::to_string(result));
+    }
+
     return result == 0;
 }
 
-bool deployProject(const std::string &deployCommand) {
-    std::cout << "Deploying project with command: " << deployCommand << std::endl;
-    return executeCommand(deployCommand);
+// Deploy the project using a list of deploy commands and return true if all deployments are successful
+bool deployProject(const std::vector<std::string> &deployCommands) {
+    bool deploymentSuccess = true;
+    for (const auto &deployCommand : deployCommands) {
+        logMessage("Deploying project with command: " + deployCommand);
+
+        if (!executeCommand(deployCommand)) {
+            deploymentSuccess = false;
+        }
+    }
+    return deploymentSuccess;
 }
